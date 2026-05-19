19f5188c0200c900d2a0bb753d5789663eb75538
Ishita Gorla <ishitagorla@gmail.com>
2026-05-19 08:52:12 +0530

    no imp change

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import { Stack, router } from 'expo-router';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function SignUp() {
   return (
